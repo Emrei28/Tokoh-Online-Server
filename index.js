d6eb63bc551155072f4f246b1f1b2d2922ac787f
Emrei28 <mreihan2804@gmail.com>
2025-08-30 15:05:48 +0700

    edit

--- index.js
+++ index.js
@@ -3,16 +3,14 @@ const cors = require('cors');
 const { Pool } = require('pg');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 require('dotenv').config();
 
 // Konfigurasi koneksi database
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false } // Penting untuk Railway
 });
 
 // Tes koneksi saat start
